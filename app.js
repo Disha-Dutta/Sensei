48c0f39ee99db8678adf051882b523141a649010
Pramod Dutta <pramoddutta_96@yahoo.com>
2017-06-30 00:21:51 +0800

    modify

--- app.js
+++ app.js
@@ -60,25 +60,28 @@ var bot = new builder.UniversalBot(connector, function (session) {
 						var temp = weather.main.temp;
 						var pressure = weather.main.pressure;
 						var humidity = weather.main.humidity;
-						var temp_min = weather.main.temp_min;
-						var temp_max = weather.main.temp_max;
+						/*var temp_min = weather.main.temp_min;
+						var temp_max = weather.main.temp_max;*/
 						console.log('temperature is '+ temp);
 						session.send('===============================================');
 						session.send('Temperature in New Delhi is : '+temp + ' degree');
 						session.send('Pressure in New Delhi is : '+pressure + ' Pascal');
 						session.send('Humidity in New Delhi is : '+ humidity );
-						session.send('Minimum temperature in New Delhi is : '+ temp_min +' Degree');
-						session.send('Maximum temperature in New Delhi is : '+ temp_max + ' Degree');
+						/*session.send('Minimum temperature in New Delhi is : '+ temp_min +' Degree');
+						session.send('Maximum temperature in New Delhi is : '+ temp_max + ' Degree');*/
 						session.send('===============================================');
 						});
 			 
 		}
 			else if(command=='news'){
 				console.log('news is working');
-				session.send('Here is your link for tech-news:'+'http://money.cnn.com/technology/');
+				var urln = 'https://en.wikipedia.org/w/api.php?action=query&list=random&format=json&rnnamespace=0&rnlimit=10';
+				request(urln,function(error,response,body){
+
+				})
 				
 				}
-    			else if(command=='How are you'){
+    			else if(command.indexOf('how')>0){
 				session.send("Great,What about you?");
 						}
 			else if(command=="Random Facts"){
